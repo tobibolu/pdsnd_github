ad9045f1895440e9e6177cb635321ac649e464a5
Tobi Bolu <tobibolu1@gmail.com>
2018-10-31 18:27:13 +0000

    Removed month index comments

--- bikeshare.py
+++ bikeshare.py
@@ -16,11 +16,11 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    
+
     city = ' '
     while city != 'Chicago' or city != 'New York City' or city != 'Washington':
         city = input("Please select either Chicago, Washington or New York City for us to analyse: ").lower()
-    
+
         if city == 'chicago':
             break
         elif city == 'new york city':
@@ -29,11 +29,11 @@ def get_filters():
             break
 
 
-    
+
     month = ' '
     while month != 'january' or month != 'february' or month != 'march' or month != 'april' or month != 'may' or month != 'june' or month != 'all':
         month = input("Please select a month between January and June or select all: ").lower()
-        
+
         if month == 'january':
             break
         elif month == 'february':
@@ -49,7 +49,7 @@ def get_filters():
         elif month == 'all':
             break
 
-    
+
     day = ' '
     while day != 'monday' or day != 'tuesday' or day != 'wednesday' or day != 'thursday' or day != 'friday' or day != 'saturday' or day != 'sunday' or day != 'all':
         day = input("Please select a day of the week or select all: ").lower()
@@ -89,29 +89,28 @@ def load_data(city, month, day):
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
-    
+
     df = pd.read_csv(CITY_DATA[city])
 
-    
+
     df['Start Time'] = pd.to_datetime(df['Start Time'])
 
-    
+
     df['month'] = df['Start Time'].dt.month
     df['day_of_week'] = df['Start Time'].dt.weekday_name
 
-    
+
     if month != 'all':
-        # use the index of the months list to get the corresponding int
         months = ['january', 'february', 'march', 'april', 'may', 'june']
         month = months.index(month) + 1
 
 
-       
+
         df = df[df['month'] == month]
 
-   
+
     if day != 'all':
-        
+
         df = df[df['day_of_week'] == day.title()]
 
     return df
@@ -123,15 +122,15 @@ def time_stats(df):
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
 
-   
+
     common_month = df['month'].mode()[0]
     print("The most popular month was %s" % common_month)
 
-    
+
     common_day = df['day_of_week'].mode()[0]
     print("The most popular day was %s" % common_day)
 
-    
+
     df['hour'] = df['Start Time'].dt.hour
     common_hour = df['hour'].mode()[0]
     print("The most popular hour was %s:00" % common_hour)
@@ -146,17 +145,17 @@ def station_stats(df):
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
 
-    
+
     popular_station = df['Start Station'].mode()[0]
     print("The station where most trips began was %s" % popular_station)
 
-    
+
     popular_end = df['End Station'].mode()[0]
     print("The station where most trips ended was %s" % popular_end)
 
 
 
-    
+
     trip_combos = df['Start Station'] + " to " + df['End Station']
     popular_trip = trip_combos.mode()[0]
     print("The most popular trip was made from %s" % popular_trip)
@@ -171,11 +170,11 @@ def trip_duration_stats(df):
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
 
-    
+
     total_travel = df['Trip Duration'].sum()
     print("The total amount of travel time is equal to {} seconds".format(int(total_travel)))
 
-    
+
     mean_travel = df['Trip Duration'].mean()
     print("The average travel time is {} seconds".format(int(mean_travel)))
 
@@ -185,14 +184,14 @@ def trip_duration_stats(df):
 
 def user_stats(df):
     """Displays statistics on bikeshare users."""
-        
+
     print('\nCalculating User Stats...\n')
     start_time = time.time()
 
     user_types = df['User Type'].value_counts()
     print("The count of different kinds of users is : %s" % user_types)
 
-    
+
     gender_count = df['Gender'].value_counts()
     print("The count of males and females is : %s" % gender_count)
 
